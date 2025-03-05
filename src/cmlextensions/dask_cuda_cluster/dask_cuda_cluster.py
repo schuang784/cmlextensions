0cf62e7229a0435eaf99c4b9f6de8773032c9fb9
pdefusco <pauldefusco@gmail.com>
2025-03-05 15:45:54 -0800

    gpu

--- src/cmlextensions/dask_cuda_cluster/dask_cuda_cluster.py
+++ src/cmlextensions/dask_cuda_cluster/dask_cuda_cluster.py
@@ -1,41 +1,14 @@
-#****************************************************************************
-# (C) Cloudera, Inc. 2020-2023
-#  All rights reserved.
+# Copyright 2025 Cloudera. All Rights Reserved.
 #
-#  Applicable Open Source License: GNU Affero General Public License v3.0
+# This file is licensed under the Apache License Version 2.0
+# (the "License"). You may not use this file except in compliance
+# with the License. You may obtain  a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0.
 #
-#  NOTE: Cloudera open source products are modular software products
-#  made up of hundreds of individual components, each of which was
-#  individually copyrighted.  Each Cloudera open source product is a
-#  collective work under U.S. Copyright Law. Your license to use the
-#  collective work is as provided in your written agreement with
-#  Cloudera.  Used apart from the collective work, this file is
-#  licensed for your use pursuant to the open source license
-#  identified above.
-#
-#  This code is provided to you pursuant a written agreement with
-#  (i) Cloudera, Inc. or (ii) a third-party authorized to distribute
-#  this code. If you do not have a written agreement with Cloudera nor
-#  with an authorized and properly licensed third party, you do not
-#  have any rights to access nor to use this code.
-#
-#  Absent a written agreement with Cloudera, Inc. (“Cloudera”) to the
-#  contrary, A) CLOUDERA PROVIDES THIS CODE TO YOU WITHOUT WARRANTIES OF ANY
-#  KIND; (B) CLOUDERA DISCLAIMS ANY AND ALL EXPRESS AND IMPLIED
-#  WARRANTIES WITH RESPECT TO THIS CODE, INCLUDING BUT NOT LIMITED TO
-#  IMPLIED WARRANTIES OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY AND
-#  FITNESS FOR A PARTICULAR PURPOSE; (C) CLOUDERA IS NOT LIABLE TO YOU,
-#  AND WILL NOT DEFEND, INDEMNIFY, NOR HOLD YOU HARMLESS FOR ANY CLAIMS
-#  ARISING FROM OR RELATED TO THE CODE; AND (D)WITH RESPECT TO YOUR EXERCISE
-#  OF ANY RIGHTS GRANTED TO YOU FOR THE CODE, CLOUDERA IS NOT LIABLE FOR ANY
-#  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR
-#  CONSEQUENTIAL DAMAGES INCLUDING, BUT NOT LIMITED TO, DAMAGES
-#  RELATED TO LOST REVENUE, LOST PROFITS, LOSS OF INCOME, LOSS OF
-#  BUSINESS ADVANTAGE OR UNAVAILABILITY, OR LOSS OR CORRUPTION OF
-#  DATA.
-#
-# #  Author(s): Paul de Fusco
-#***************************************************************************/
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+# OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+# License for the specific permissions and limitations governing your
+# use of the file.
 
 import os
 import cdsw
@@ -50,6 +23,7 @@ class DaskCudaCluster:
         self,
         num_workers,
         worker_cpu,
+        nvidia_gpu,
         worker_memory,
         scheduler_cpu,
         scheduler_memory,
@@ -57,6 +31,7 @@ class DaskCudaCluster:
     ):
         self.num_workers = num_workers
         self.worker_cpu = worker_cpu
+        self.nvidia_gpu = nvidia_gpu
         self.worker_memory = worker_memory
         self.scheduler_cpu = scheduler_cpu
         self.scheduler_memory = scheduler_memory
@@ -71,6 +46,7 @@ class DaskCudaCluster:
         args = {
             'n': 1,
             'cpu': self.scheduler_cpu,
+            'nvidia_gpu': self.nvidia_gpu,
             'memory': self.scheduler_memory,
             'code': dask_scheduler_cmd,
         }
@@ -91,6 +67,7 @@ class DaskCudaCluster:
             'n': self.num_workers,
             'cpu': self.worker_cpu,
             'memory': self.worker_memory,
+            'nvidia_gpu': self.nvidia_gpu,
             'code': worker_start_cmd,
         }
 
